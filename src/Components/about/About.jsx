d3f6cb8a101b8b1d2e35f10781c2fa1cd542b65a
ajay <achmadfajar712@gmail.com>
2026-03-09 01:07:20 +0700

    change about section

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -25,7 +25,9 @@ const About = () => {
     <section ref={trigger} className='p-section my-20 xl:my-40 w-full min-h-[100vh] xl:h-[100vh] grid grid-cols-1 grid-rows-5 md:grid-cols-3 md:grid-rows-2 gap-4'>
         <div className='mb-2'>
             <h1 className='mt-8 text-4xl font-raleway uppercase font-bold flex gap-1'>Tentang Kami <span className='font-poppins text-sm'>(1)</span></h1>
-            <p ref={aboutText} className='mt-4 text-xl font-poppins'>Kami adalah penyedia layanan pembuatan website yang berfokus pada kualitas, strategi, dan ketepatan fungsi. Setiap website yang kami kembangkan dirancang untuk merepresentasikan identitas brand Anda secara profesional serta mendukung kebutuhan bisnis secara berkelanjutan.</p>
+            <p ref={aboutText} className='mt-4 text-xl font-poppins'>
+              Kami adalah tim yang berfokus pada pengembangan website berkualitas dengan pendekatan strategi dan desain yang tepat, untuk membantu brand Anda tampil profesional dan berkembang secara berkelanjutan.
+            </p>
             <div className='flex items-center gap-4'>
                 <button className='mt-4 rounded-[20px] bg-orange-300 text-black/80 px-6 py-2'>Kontak Kami</button>
                 <button className='mt-4 rounded-[20px] bg-black/80 text-white px-6 py-2'>Lihat Projek</button>
