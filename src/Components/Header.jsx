2343de9eec6f30ac3666e86d7ae04360269a220e
ajay <achmadfajar712@gmail.com>
2026-03-10 01:15:25 +0700

    change size text for header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <header className='px-4 md:px-8 xl:px-20 py-4 fixed top-0 w-full flex justify-between items-center z-40 glassmorphism'>
         <div className='z-44 '>
-            <h1 className='text-xl md:text-4xl font-raleway -mb-1 font-bold'>SSH</h1>
-            <p className="text-xs md:text-[16px]">Surupan Software House</p>
+            <h1 className='text-xl md:text-2xl font-raleway -mb-1 font-bold'>SSH</h1>
+            <p className="text-xs md:text-sm">Surupan Software House</p>
         </div>
-        <div className='md:text-xl font-raleway uppercase'>
+        <div className='md:text-[16px] font-raleway uppercase'>
             Menu
         </div>
     </header>
