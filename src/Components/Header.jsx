f4a74b27dc197d8617cc7734143ebcf7e1c6cd1e
ajay <achmadfajar712@gmail.com>
2026-03-10 20:47:59 +0700

    change size of text on navbar while mobile version

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
         <div className="bg-gray-100 shadow-md flex items-center mt-4 px-6 py-2 font-poppins gap-4 xl:gap-20 rounded-[22px]">
             <h1 className="font-bold px-4 xl:text-2xl font-raleway rounded-[12px] text-black/80">SSH</h1>
             <nav>
-              <ul className="flex items-center gap-2 text-sm md:text-[16px] xl:gap-4 text-black/80">
+              <ul className="flex items-center gap-2 text-xs md:text-[16px] xl:gap-4 text-black/80">
                 <li className="bg-black/80 text-white px-4 py-2 rounded-[12px]">Home</li>
                 <li className=" px-4 py-2 rounded-[12px]">Portfolio</li>
                 <li className=" px-4 py-2 rounded-[12px]">Blog</li>
